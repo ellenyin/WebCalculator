ebc127bf4afb2395dd56097553dd23d3d9937846
Ellen Yin <ellen@epsilamic.ca>
2018-01-06 22:02:39 -0500

    Add numberClick function and addEvenListener into initialize function

--- WebCalculator/Scripts/CalculatorLibary.js
+++ WebCalculator/Scripts/CalculatorLibary.js
@@ -2,4 +2,28 @@
 
 function initialize() {
     txtInput = document.getElementById('txtInput');
+    document.getElementById('btn5').addEventListener('click', numberClick, false);
 }
+
+function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+        this.innerText : txtInput.value + this.innerText;
+}
+
+/*function numberClick2() {
+    if (txtInput.value == '0') {
+        txtInput.value = this.innerText;
+    } else {
+        txtInput.value = txtInput.value + this.innerText;
+    }
+}
+
+function numberClick3() {
+    txtInput.value = (function () {
+        if (txtInput.value == '0') {
+            return this.innerText;
+        } else {
+            return txtInput.value + this.innerText;
+        }
+    })();
+}*/
