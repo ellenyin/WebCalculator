b157631ec7273e02bac359142e8e361c8aac3bad
Ellen Yin <ellen@epsilamic.ca>
2018-01-10 16:33:39 -0500

    Show the result by clicking equal button

--- WebCalculator/Scripts/CalculatorLibary.js
+++ WebCalculator/Scripts/CalculatorLibary.js
@@ -12,46 +12,65 @@ function initialize() {
     document.getElementById('btnMinus').addEventListener('click', minusClick, false);
     document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
     document.getElementById('btnClear').addEventListener('click', clear, false);
+    document.getElementById('btnequal').addEventListener('click', equal, false);
     clear();
 }
-function plusClick() {
-    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
-    clearEntry();
-}
-function minusClick() {
-    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
-    clearEntry();
-}
-
-function numberClick() {
-    txtInput.value = txtInput.value == '0' ?
-        this.innerText : txtInput.value + this.innerText;
-}
-
-function clearEntry() {
-    txtInput.value = '0';
-}
-
-function clear() {
-    txtInput.value = '0';
-    txtResult.value = '0';
-}
-
-
-/*function numberClick2() {
-    if (txtInput.value == '0') {
-        txtInput.value = this.innerText;
-    } else {
-        txtInput.value = txtInput.value + this.innerText;
+function equal() {
+    if (operation == 0) {
+        txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+        txtInput.value = '0';
+    }
+    else if (operation == 1) {
+        txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+        txtInput.value = '0';
     }
 }
 
-function numberClick3() {
-    txtInput.value = (function () {
+function plusClick() {
+    operation = 0;
+    txtResult.value = txtInput.value;
+    txtInput.value = '0';
+}
+
+
+function minusClick() {
+    operation = 1;
+
+    txtResult.value = txtInput.value;
+    txtInput.value = '0';
+
+}
+
+    function numberClick() {
+        txtInput.value = txtInput.value == '0' ?
+            this.innerText : txtInput.value + this.innerText;
+        console.log('Hello');
+    }
+
+    function clearEntry() {
+        txtInput.value = '0';
+    }
+
+    function clear() {
+        txtInput.value = '0';
+        txtResult.value = '0';
+    }
+
+
+    /*function numberClick2() {
         if (txtInput.value == '0') {
-            return this.innerText;
+            txtInput.value = this.innerText;
         } else {
-            return txtInput.value + this.innerText;
+            txtInput.value = txtInput.value + this.innerText;
         }
-    })();
-}*/
+    }
+    
+    function numberClick3() {
+        txtInput.value = (function () {
+            if (txtInput.value == '0') {
+                return this.innerText;
+            } else {
+                return txtInput.value + this.innerText;
+            }
+        })();
+    }*/
